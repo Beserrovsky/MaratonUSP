15900911ed3c2616a38503afafde4520a004e998
Beserrovsky <felipebeserra25@gmail.com>
2022-04-04 23:07:47 -0300

    Just a simple string test

---
+++ src/day2/strings.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string teste = "Felipe";
+    
+    for(int i = 0; i < teste.size(); i++)
+        cout << teste[i];
+    cout << endl;
+}
